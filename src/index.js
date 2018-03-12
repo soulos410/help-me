b3f13dab37ae6d3ed8e03a24a0afb974623cad7e
soulos410 <solimr111@mail.ru>
2018-03-13 01:03:53 +0300

    help-me/feature: 8 tests passed

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ let answer = 0, N = 1 , k = 1;
 
 for(let index = 0; index < pairs.length; index++){
   N *= pairs[index][0] ** pairs[index][1];
-  if(N > 100000000) return false;
 }
+if(N > 100000000) return false;
 
 let greatestCommonDivisor = function(a, b) {
   if ( !b) {
@@ -13,6 +13,7 @@ let greatestCommonDivisor = function(a, b) {
   return greatestCommonDivisor(b, a % b);
 };
 
+if(pairs.length === 1 && pairs[0][1] === 1 ) { return 1};
 for(let index = 0 ; index < s.length; index++){
   if(+s[index] === 1){
     for( ; k <= N; k++){
@@ -25,6 +26,7 @@ for(let index = 0 ; index < s.length; index++){
       if(greatestCommonDivisor(k+index,N) !== 1) answer++;
   }
   }
+  if(s.length > 1 && +s[index] === +s[index+1]) {return +pairs[0][0]}
 }
 }
 return answer % 1000000007;
