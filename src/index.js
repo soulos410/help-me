f6151e49dc98b2a1ffa8c7de076ceeb4d42e71ce
soulos410 <solimr111@mail.ru>
2018-03-14 00:43:55 +0300

    help-me/feature: started from the beginning. 11 test passed

--- src/index.js
+++ src/index.js
@@ -1,33 +1,44 @@
 module.exports = function count(s, pairs) {
-let answer = 0, N = 1 , k = 1;
+  let answer = 0, N = 1 , isRight;
 
-for(let index = 0; index < pairs.length; index++){
-  N *= pairs[index][0] ** pairs[index][1];
-}
-if(N > 100000000) return false;
-
-let greatestCommonDivisor = function(a, b) {
-  if ( !b) {
-      return a;
+  for(let index = 0; index < pairs.length; index++){
+    N *= pairs[index][0] ** pairs[index][1];
   }
-  return greatestCommonDivisor(b, a % b);
-};
+  if(N > 100000000) return false;
 
-if(pairs.length === 1 && pairs[0][1] === 1 ) { return 1};
-for(let index = 0 ; index < s.length; index++){
-  if(+s[index] === 1){
-    for( ; k <= N; k++){
-      if(greatestCommonDivisor(k+index,N) === 1) answer++;    
+  let greatestCommonDivisor = function(a, b) {
+    if ( !b) {
+        return a;
     }
+    return greatestCommonDivisor(b, a % b);
+  };
+
+  let checkOne = function(a,b){
+    return greatestCommonDivisor(a,b) === 1 ? true : false;
   }
-  else{
-    if(+s[index] === 0){
-      for( ; k <= N; k++){
-      if(greatestCommonDivisor(k+index,N) !== 1) answer++;
+
+  let checkZero = function(a,b){
+    return greatestCommonDivisor(a,b) !== 1 ? true : false;
   }
-  }
-  if(s.length > 1 && +s[index] === +s[index+1]) {return +pairs[0][0]}
+ 
+  for (let k = 0; k < N; k++) {
+    for (let j = 0; j < s.length; j++) {
+        if (+s[j] === 1) {
+            if (checkOne(k+j,N)) isRight = true;
+              else {
+                isRight = false;
+                break;
+              }
+        } 
+        if (+s[j] === 0) {
+            if (checkZero(k+j,N)) isRight = true;
+              else {
+                isRight = false;
+                break;
+              }
+        }
+    }
+    isRight === true ? answer++ : answer+=0;
 }
-}
-return answer % 1000000007;
+  return answer % 1000000007;
 }
